901cad65f8483bc4d91758facaee1734747255df
Marian Dumitru Danci <34145543+dumitrux@users.noreply.github.com>
2024-01-02 10:26:09 +0000

    Fix github path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://dumitrux.github.io',
-  // base: '/thenotebookcorner.com',
+  base: '/thenotebookcorner.com'
 });
